a1c2bc34a29841497b9344d6f7311c277d95adcc
GiffaIndr <giffasyaikhani2005@gmail.com>
2023-01-05 23:19:03 +0700

    new commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const sr = ScrollReveal({
+    distance:'40px',
+    duration: 2600,
+    reset: true
+ })
+ sr.reveal('.home-text',{delay:300,origin:'left'})
+ sr.reveal('.about,.portfolio,.project, .sertifikat,.social-media',{delay:100, origin:'bottom'})
+
